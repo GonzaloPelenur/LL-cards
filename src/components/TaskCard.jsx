f7a397bc920e124a2b73e5ade370b2b53e932adc
GonzaloPelenur <gpelenur@college.harvard.edu>
2023-04-20 16:56:35 -0400

    task card looks better

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,78 +1,95 @@
 import { Container, Card, Grid, Text, Button, Row, Spacer, Badge, Col } from "@nextui-org/react";
-import Image from "next/image"
-import photoshopPic from "../images/photoshop.png"
-import dinoPic from "../images/dino.jpeg"
 export const config = { unstable_fastRefresh: false }
 
+const target_to_color = {
+    "LLUF": "secondary",
+    "MPA": "warning",
+    "MDA": "primary"
+}
 
-const list = [
-    {
-        title: "Photoshop",
-        img: photoshopPic,
-        price: "",
-    },
-    {
-        title: "Blender",
-        img: photoshopPic,
-        price: "",
-    }
-];
+const tool_to_color = {
+    "Photoshop": "primary",
+    "After Effects": "secondary",
+    "Blender": "success",
+    "React": "warning",
+    "Figma": "error",
+    "Canva": "primary",
+    "Illustrator": "secondary"
+}
 // console.log(process.cwd())
 export function TaskCard(prop) {
     // console.log(data)
-    const { Name, Target, Description, Deadline } = prop.data;
+    const { Name, Target, Description, Deadline, Tools } = prop.data;
     var deadline_f = new Date(Deadline);
     const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
 
     return (
 
-        <Card css={{ mw: "440px" }}>
+        <Card css={{ mw: "440px" }} >
             <Card.Header>
-                <Text h2>{Name}</Text>
+                <Grid.Container>
+                    <Row>
+                        <Text h2>{Name}</Text>
+                    </Row>
+                    <Row>
+                        <Badge size="sm" color="primary" variant="flat">{deadline_f.toLocaleDateString('en-US', dateOptions)}</Badge>
+                    </Row>
+                </Grid.Container>
 
             </Card.Header>
             <Card.Divider />
             <Card.Body css={{ py: "$10" }}>
-                <Text h4>Deadline:</Text>
-                <Badge>{deadline_f.toLocaleDateString('en-US', dateOptions)}</Badge>
-                <Spacer y={1} />
-                <Text h5>
-                    {Description}
-                </Text>
-                {/* <Image
-                    src={dinoPic}
-                    alt="Dino"
-                    width={300}
-                    height={200}
-                /> */}
-                <Spacer y={1} />
-                <Card>
-                    <Card.Body>
-                        <Text h3>Available for:</Text>
-                        {Target.map((item, index) => (
-                            <Badge>{item}</Badge>
-                        ))}
-
-                    </Card.Body>
-                </Card>
-            </Card.Body>
-            <Card.Divider />
-            <Card.Footer>
-                <Container>
+                <Grid.Container gap={1}>
                     <Row>
-
-                        {list.map((item, index) => (
-
-                            <Col key={index}>
-                                <Image src={item.img}
-                                    alt={item.title}
-                                    width={50}
-                                    height={50} />
-                            </Col>
-                        ))}
+                        <Grid>
+                            <Text h4>
+                                {Description}
+                            </Text>
+                        </Grid>
                     </Row>
-                </Container>
-            </Card.Footer>
+                    <Row>
+                        <Grid>
+                            <Grid.Container>
+
+                                <Row >
+                                    <Text h3>Available For</Text>
+                                </Row>
+                                <Row>
+                                    {Target.map((item, index) => (
+                                        <Grid>
+                                            <Badge color={target_to_color[item]}>{item}</Badge>
+                                        </Grid>
+                                    ))}
+                                </Row>
+
+
+                            </Grid.Container>
+                        </Grid>
+                    </Row>
+                    <Row>
+                        <Grid>
+                            <Grid.Container>
+
+                                <Row >
+                                    <Text h3>Tools</Text>
+                                </Row>
+                                <Row>
+                                    {Tools.map((item, index) => (
+                                        <Grid>
+                                            <Badge color={tool_to_color[item]}>{item}</Badge>
+                                        </Grid>
+                                    ))}
+                                </Row>
+
+
+                            </Grid.Container>
+                        </Grid>
+                    </Row>
+
+                </Grid.Container>
+
+            </Card.Body>
+
         </Card >
 
     );
---
+++ src/pages/tasks.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+import Airtable from 'airtable';
+import { Inter } from 'next/font/google'
+import { Container, Grid, Spacer } from "@nextui-org/react";
+import { TaskCard } from "../components/TaskCard"
+
+// require('dotenv').config()
+
+const inter = Inter({ subsets: ['latin'] })
+
+export async function getStaticProps() {
+  const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_MENU_BASE);
+  const records = await base('Tasks').select({ view: 'MAIN' }).all();
+  // console.log(JSON.stringify(records, null, 4))
+  const fields = records
+    .filter(item => Object.keys(item.fields).length !== 0) // exclude items where fields is empty
+    .map(item => item.fields);
+  // console.log(fields)
+
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(fields)),
+    },
+  };
+}
+
+const theme = createTheme({
+  type: "light", // it could be "light" or "dark"
+  theme: {
+    colors: {
+      // brand colors
+      background: '#e3f5ff',
+
+
+      // ...  more colors
+    },
+    space: {},
+    fonts: {}
+  }
+})
+
+export default function Tasks({ data }) {
+
+  return (
+    <NextUIProvider theme={theme}>
+      <Container justify='center' display='flex' alignItems='center' >
+        <Spacer y={2} />
+        <Grid gap={2} sm={12} md={15}>
+          <Spacer y={2} />
+          {data.map((item, index) => (
+            <>
+              <TaskCard data={item} key={index} />
+              <Spacer x={1} />
+            </>
+
+          ))}
+        </Grid>
+      </Container>
+    </NextUIProvider >
+  )
+}
